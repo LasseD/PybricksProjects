1849da12d654ac537c595b0764669ddbe754d30c
Lasse Deleuran <lasse.deleuran@lego.com>
2024-03-25 09:32:41 +0100

    Removed circling lights and made Garbage truck arm work more reliably.

--- 42167_Mack_LR_Electric_Garbage_Truck.py
+++ 42167_Mack_LR_Electric_Garbage_Truck.py
@@ -1,26 +1,33 @@
+from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, Remote
 from pybricks.parameters import Button, Port, Color
 from pybricks.tools import wait, StopWatch
 
+hub = PrimeHub()
+hub.light.off()
+hub.display.off() # No circular lights on this model.
 remote = Remote()
 arm = Motor(Port.B)
 steer = Motor(Port.D)
 drive = Motor(Port.F)
 ACCELERATION = 1.8
-SPEED_ARM = 150
+SPEED_ARM = 190
 STEERING_BOOST = 1.7
+MIN_DC = 20
+TIME_TURN_OFF = 2000
+#LIFT_TOP = -72
+LIFT_TOP = -60
 greenButtonHoldTime = StopWatch()
 greenButtonDown = False
-TIME_TURN_OFF = 2000
 speed = 0
 
 def runArm():
     remote.light.on(Color.RED)
-    arm.run_target(SPEED_ARM, -70)
-    arm.run_target(2*SPEED_ARM, -60)
-    arm.run_target(2*SPEED_ARM, -70)
-    arm.run_target(2*SPEED_ARM, -60)
-    arm.run_target(2*SPEED_ARM, -70)
+    arm.run_target(SPEED_ARM, LIFT_TOP)
+    arm.run_target(2*SPEED_ARM, LIFT_TOP+15)
+    arm.run_target(2*SPEED_ARM, LIFT_TOP)
+    arm.run_target(2*SPEED_ARM, LIFT_TOP+15)
+    arm.run_target(2*SPEED_ARM, LIFT_TOP)
     arm.run_target(SPEED_ARM, 120)
     remote.light.on(Color.GREEN)
 
@@ -40,13 +47,13 @@ while True:
     if Button.LEFT in buttons:
         speed = 0
     elif Button.LEFT_PLUS in buttons:
-        speed = min(100, speed+2*ACCELERATION)
+        speed = max(MIN_DC, min(100, speed+ACCELERATION))
     elif Button.LEFT_MINUS in buttons:
-        speed = max(-100, speed-ACCELERATION)
+        speed = min(-MIN_DC, max(-100, speed-ACCELERATION))
     elif speed > 0:
-        speed = max(0, speed - ACCELERATION)
+        speed = max(0, speed - 2*ACCELERATION)
     else:
-        speed = min(0, speed + ACCELERATION)
+        speed = min(0, speed + 2*ACCELERATION)
     drive.dc(speed)
 
     # Steering:
@@ -57,7 +64,10 @@ while True:
     else:
         steeringTarget = 0
     steeringDiff = steeringTarget-steer.angle()
-    steer.dc(min(100, max(-100, STEERING_BOOST*steeringDiff)))
+    steerDC = min(100, max(-100, STEERING_BOOST*steeringDiff))
+    if abs(steerDC) < 10:
+        steerDC = 0
+    steer.dc(steerDC)
 
     # Turn off using green button:
     if Button.CENTER in buttons:
